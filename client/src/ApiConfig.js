9e5a357269434303580cc38866c03654d1fbe051
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-06-19 20:12:37 +0530

    Added config file

--- client/src/ApiConfig.js
+++ client/src/ApiConfig.js
@@ -1,10 +1,7 @@
 
-let BASE;
+let BASE = '';
 if(process.env.NODE_ENV == 'development') {
     BASE = 'http://ec2-13-126-19-80.ap-south-1.compute.amazonaws.com/';
-}
-else if(process.env.NODE_ENV == 'production') {
-    BASE = 'http://127.0.0.1/'
 }
 
 const BASE_URL = `${BASE}api/v1/`
