0700f7c8ce802707f46a1d8de84723e1139f7ee0
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-06-20 21:14:01 +0530

    Iterated over Live objects

--- client/src/containers/HomeScreen/HomeScreen.js
+++ client/src/containers/HomeScreen/HomeScreen.js
@@ -10,43 +10,29 @@ import nonveg from '../../assets/nonveg.png';
 class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0
-    };
     this.props.getMenu();
   }
-  updateInputValue(val) {
-    const re = /^[0-9\b]+$/;
-    if (val === '' || re.test(val)) {
-      this.setState({
-        count:val
-      })
-    }
+
+  renderCards () {
+    const menu = this.props.menu;
+    return (menu.menu.map((element,i)=>{
+      return <Card
+        props={element}
+        count={menu.itemsQuantity[i]}
+        plus={() => this.props.incrementQuantity(i)}
+        minus={() => this.props.decrementQuantity(i)}
+        updateInputValue={(e)=>this.props.updateItem(e,i)}
+      />
+    }));
   }
-  handleIncreament() {
-    const currentVal = (isNaN(parseInt(this.state.count)))?0:parseInt(this.state.count);
-    this.setState({
-      count: currentVal + 1
-    });
-  }
-  handleDecreament() {
-    this.setState({
-      count: this.state.count > 0 ? this.state.count - 1 : 0
-    });
-  }
+
   render() {
     if(!this.props.menu.loaded)
       return <div>Loading...</div>;
-    console.log("the menu integration is.....", this.props.menu);
-    const props = this.props.menu.menu[0];
     return (
-        <Card
-          props={props}
-          count={this.props.menu.itemsQuantity[0]}
-          plus={() => this.props.incrementQuantity(0)}
-          minus={() => this.props.decrementQuantity(0)}
-          updateInputValue={(e)=>this.props.updateItem(e,0)}
-        />
+        <div>
+          {this.renderCards()}
+        </div>
     );
   }
 }
