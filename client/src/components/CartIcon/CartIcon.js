b5dba50a608d2d0429ea08006a2772201bd542bb
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-06-22 17:57:50 +0530

    Modified navbar component

--- client/src/components/CartIcon/CartIcon.js
+++ client/src/components/CartIcon/CartIcon.js
@@ -3,10 +3,14 @@ import { connect } from 'react-redux';
 import { FaShoppingCart } from "react-icons/fa";
 
 const CartIcon = props => (
-    <button style={{ display: "flex", flexDirection: "column", alignItems: "flex-end", background: "none", border: "none", outline: "none", color: "white" }}>
-        <div style={{ display: "flex", backgroundColor: 'black', borderRadius: 13, width: 25, height: 25, zIndex: 2, marginRight: -12, marginBottom: -10, justifyContent: 'center', alignItems: 'center' }}>
-            <div style={{ color: 'white' }}>{props.cartCount}</div>
-        </div>
+    <button style={{ display: "flex", flexDirection: "column", alignItems: "flex-end", background: "none", border: "none", outline: "none", color: "white" }}
+        onClick={() => props.cartNavigate()}>
+        {
+            props.cartCount != 0 &&
+            <div style={{ display: "flex", backgroundColor: 'black', borderRadius: 13, width: 25, height: 25, zIndex: 2, marginRight: -12, marginBottom: -10, justifyContent: 'center', alignItems: 'center' }}>
+                <div style={{ color: 'white' }}>{props.cartCount}</div>
+            </div>
+        }
         <FaShoppingCart size={30} />
     </button>
 )
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,13 +1,18 @@
 import React from "react";
 import CartIcon from "../CartIcon/CartIcon";
 
-const NavBar = () => (
-  <div style={{ backgroundColor: "#CB202D", color: "white", width: "100%", position: "fixed", top: 0, left: 0, zIndex: 70, height: 70, padding: 5 }}>
-    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginLeft: 150, marginRight: 150, paddingLeft:20,paddingRight:20, height: "100%" }}>
-      <img src={require('../../assets/logo.png')} style={{height:70}}/>
-      <CartIcon/>
+const NavBar = (props) => {
+  return (
+    <div style={{ backgroundColor: "#CB202D", color: "white", width: "100%", position: "fixed", top: 0, left: 0, zIndex: 70, height: 70, padding: 5 }}>
+    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginLeft: 270, marginRight: 270, paddingLeft: 20, paddingRight: 20, height: "100%" }}>
+      <img src={require('../../assets/logo.png')} style={{ height: 70 }} />
+      {props.cart==false ||
+        <CartIcon
+          cartNavigate={null}
+        />}
     </div>
   </div>
-)
+  );
+}
 
 export default NavBar;
