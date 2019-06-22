f85b11210614be9d08d1bca40f3e3b1aa1e53730
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-06-23 00:49:41 +0530

    Added redux persist

--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,41 @@ import createSagaMiddleware from 'redux-saga';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { logger } from 'redux-logger';
-import reducer from './reducers';
+import rootReducer from './reducers';
 import App from './containers/App';
 import rootSaga from './sagas';
 
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  reducer,
+  persistedReducer,
   applyMiddleware(sagaMiddleware, logger),
 );
+const persistor = persistStore(store)
+
+
 
 sagaMiddleware.run(rootSaga);
 
 render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   ,
   document.getElementById('root'),
