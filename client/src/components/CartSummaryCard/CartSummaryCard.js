ab03a66cdee4cfed8f585cd770a422d671cfe6b7
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-06-23 22:27:07 +0530

    Increment to cart functionality

--- client/src/components/CartSummaryCard/CartSummaryCard.js
+++ client/src/components/CartSummaryCard/CartSummaryCard.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { connect } from 'react-redux';
 
 import veg from '../../assets/veg.png';
 import nonveg from '../../assets/nonveg.png';
-// import AddToCartButton from '../AddToCartButton/AddToCartButton';
-import { getMenu, incrementQuantity, decrementQuantity, updateItem, addToCart } from '../../actions';
 import ItemIncrementor from '../ItemIncrementor/ItemIncrementor';
 
 
 const CartSummaryCard = data => {
-    const { cartItem, quantity, count, plus,minus } = data;
+    const { cartItem, quantity, count, plus,minus,addToCart } = data;
     const mark = cartItem.type == "veg" ? veg : nonveg;
     const netPrice = cartItem.price * quantity;
-    const i = 0;
     return (
         <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", flex: 1, height: 224, marginBottom: 20 }}>
             <div style={{ display: "flex", alignItems: "center" }}>
@@ -27,7 +23,8 @@ const CartSummaryCard = data => {
                     count={count}
                     plus={plus}
                     minus={minus}
-                    updateInputValue={false}
+                    cart
+                    addToCart={(data) => addToCart(data)}
                 />
             </div>
             <div>Price/Plate:{cartItem.price}</div>
@@ -36,18 +33,4 @@ const CartSummaryCard = data => {
     );
 };
 
-const mapDispatchToProps = {
-    getMenu: getMenu,
-    incrementQuantity: incrementQuantity,
-    decrementQuantity: decrementQuantity,
-    updateItem: updateItem,
-    addToCart: addToCart
-};
-
-function mapStateToProps(state) {
-    return {
-        menu: state.menu,
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CartSummaryCard);
+export default CartSummaryCard;
--- client/src/components/ItemIncrementor/ItemIncrementor.js
+++ client/src/components/ItemIncrementor/ItemIncrementor.js
@@ -3,43 +3,57 @@ import { FiPlusCircle, FiMinusCircle } from "react-icons/fi";
 
 
 const ItemIncrementor = props => {
-    const {minus, plus, count} = props;
-    return (
-        <div style={{ display: "flex" }}>
-            <p>Qty</p>
-            <button
-                style={{ background: "none", border: "none", outline: "none" }}
-                onClick={() => minus()}
-            >
-                <FiMinusCircle />
-            </button>
-            {
-                props.updateInputValue == false ?
+    const { minus, plus, count, cart, addToCart } = props;
+    return cart ?
+        (
+            <div style={{ display: "flex" }}>
+                <p>Qty</p>
+                <button
+                    style={{ background: "none", border: "none", outline: "none" }}
+                    onClick={() => addToCart(count-1)}
+                >
+                    <FiMinusCircle />
+                </button>
                 <div>
                     {count}
-                </div>:
+                </div>
+                <button
+                    style={{ background: "none", border: "none", outline: "none" }}
+                    onClick={() => addToCart(count+1)}
+                >
+                    <FiPlusCircle />
+                </button>
+            </div>
+        ) :
+        (
+            <div style={{ display: "flex" }}>
+                <p>Qty</p>
+                <button
+                    style={{ background: "none", border: "none", outline: "none" }}
+                    onClick={() => minus()}
+                >
+                    <FiMinusCircle />
+                </button>
                 <input
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    margin: 10,
-                    width: 30,
-                    textAlign: "center"
-                }}
-                value={count != 0 ? count : 1}
-                onChange={evt => props.updateInputValue(evt.target.value)}
-            />
-            }
-            
-            <button
-                style={{ background: "none", border: "none", outline: "none" }}
-                onClick={() => plus()}
-            >
-                <FiPlusCircle />
-            </button>
-        </div>
-    );
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        margin: 10,
+                        width: 30,
+                        textAlign: "center"
+                    }}
+                    value={count != 0 ? count : 1}
+                    onChange={evt => props.updateInputValue(evt.target.value)}
+                />
+                <button
+                    style={{ background: "none", border: "none", outline: "none" }}
+                    onClick={() => plus()}
+                >
+                    <FiPlusCircle />
+                </button>
+            </div>
+        );
 }
 
 export default ItemIncrementor;
