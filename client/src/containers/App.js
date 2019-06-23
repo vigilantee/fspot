0bd334045f8b21825f7f84c9e23c79f1961e3bda
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-06-24 03:37:32 +0530

    Testing Sign In

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import HomeScreen from './HomeScreen/HomeScreen';
 import CartSummary from './CartSummary/CartSummary';
+import LoginPage from './LoginPage/LoginPage';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/" component={LoginPage} />
         <Route path="/cart" component={CartSummary} />
       </Router>
     );
--- client/src/containers/LoginPage/LoginPage.js
+++ client/src/containers/LoginPage/LoginPage.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { GoogleLogin } from 'react-google-login';
- 
- 
+
+
 const responseGoogle = (response) => {
-  console.log(response);
+    console.log(response);
 }
- 
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
 ReactDOM.render(
-  <GoogleLogin
-    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com241827298425-214lin8vtu0unf3b3qeda2017v2vl75n.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />,
-  document.getElementById('googleButton')
+    <GoogleLogin
+        clientId="241827298425-5gc7bo15mt1vjhqrbi49l9svn9g4lkpk.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+    />,
+    document.getElementById('googleButton')
 );
 
 
