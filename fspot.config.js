a67d519c07a5db170847701581051a71c36ef29c
Ubuntu <ubuntu@ip-172-31-35-98.ap-south-1.compute.internal>
2019-07-07 11:47:56 +0000

    Added config

--- fspot.config.js
+++ fspot.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
       name        : "fspot-api",
-      script      : "./server/server.js",
+      script      : "./server/startjs",
       watch       : true,
       env: {
         "NODE_ENV": "development",
