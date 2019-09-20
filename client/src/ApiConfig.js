9577d77cb1e4c003eb5c518eac6c99000c11901c
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-09-20 19:30:22 +0530

    Error noted

--- client/src/ApiConfig.js
+++ client/src/ApiConfig.js
@@ -1,13 +1,13 @@
 
 let BASE = '';
 let BASE_URL = '';
-if(process.env.NODE_ENV == 'development') {
-    BASE = 'http://localhost:4000/';
-    BASE_URL = `${BASE}v1/`;
-}
-else {
+// if(process.env.NODE_ENV == 'development') {
+//     BASE = 'http://localhost:4000/';
+//     BASE_URL = `${BASE}v1/`;
+// }
+// else {
     BASE = 'http://ec2-13-126-19-80.ap-south-1.compute.amazonaws.com/';
     BASE_URL = `${BASE}api/v1/`;
-}
+// }
 
 export default BASE_URL;
