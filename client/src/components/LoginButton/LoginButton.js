ba9926fa1e2f4b4380e041fbfa1b2a22de0d02b5
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-09-20 19:31:34 +0530

    Error noted

--- client/src/components/LoginButton/LoginButton.js
+++ client/src/components/LoginButton/LoginButton.js
@@ -13,7 +13,7 @@ class LoginButton extends React.Component {
       this.props.signInSuccess(response);
     }
     this.failedSignin = (error) => {
-      console.log("Error occured", e)
+      console.log("Error occured", error);
     }
   }
 
