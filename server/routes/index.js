a554d411f65368b916dcaaf952f5136a2f95e5f4
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-09-20 21:11:11 +0530

    Added alignment

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,69 +1,69 @@
-const express=require('express');
-const db=require('../db/index.js');
-const router=express.Router();
+const express = require('express');
+const db = require('../db/index.js');
+const router = express.Router();
 
-router.post('/order',(req,res,next)=>{
-    try{
+router.post('/order', (req, res, next) => {
+    try {
         const data = req.body;
-        if(!data.user_id||!data.total) throw "User ID or Total is not defined";
-        db.executeQuery(`insert into orders(userId,total,totalDiscount,cgst,sgst)values(${data.userId},${data.total},${data.totalDiscount?data.totalDiscount:0},${data.cgst?data.cgst:0},${data.sgst?data.sgst:0})`,results=>res.json({response:"Order successfully placed",order_id:results.insertId}));
-    }catch(e){
-        console.log('Error logged....',e);
+        if (!data.user_id || !data.total) throw "User ID or Total is not defined";
+        db.executeQuery(`insert into orders(userId,total,totalDiscount,cgst,sgst)values(${data.userId},${data.total},${data.totalDiscount ? data.totalDiscount : 0},${data.cgst ? data.cgst : 0},${data.sgst ? data.sgst : 0})`, results => res.json({ response: "Order successfully placed", order_id: results.insertId }));
+    } catch (e) {
+        console.log('Error logged....', e);
         res.sendStatus(500);
     }
 });
 
-router.get('/menu',(req,res,next)=>{
-    try{
+router.get('/menu', (req, res, next) => {
+    try {
         let filters = '1 ';
-        if(req.query.veg)
+        if (req.query.veg)
             filters += 'and type = "veg" ';
-        else if(req.query.nonveg)
+        else if (req.query.nonveg)
             filters += 'and type = "non-veg" ';
-        db.executeQuery(`select * from product where ${filters}`, results=>res.json(results));
-    }catch(e){
-        console.log('Error logged....',e);
+        db.executeQuery(`select * from product where ${filters}`, results => res.json(results));
+    } catch (e) {
+        console.log('Error logged....', e);
         res.sendStatus(500);
     }
 });
 
-router.post('/addtocart',(req,res,next)=>{
-    try{
+router.post('/addtocart', (req, res, next) => {
+    try {
         let items = req.body.items;
         let user_id = req.body.userid;
-        if(req.query.veg)
+        if (req.query.veg)
             filters += 'and type = "veg" ';
-        else if(req.query.nonveg)
+        else if (req.query.nonveg)
             filters += 'and type = "non-veg" ';
-        db.executeQuery(`select * from product where ${filters}`, results=>res.json(results));
-    }catch(e){
-        console.log('Error logged....',e);
+        db.executeQuery(`select * from product where ${filters}`, results => res.json(results));
+    } catch (e) {
+        console.log('Error logged....', e);
         res.sendStatus(500);
     }
 });
 
-router.get('/cocodevs',(req,res,next)=>{
-    try{
-        db.executeQuery(`select * from cocodevs order by id desc`,results=>res.json(results));
-    }catch(e){
-        console.log('Error logged......',e);
+router.get('/cocodevs', (req, res, next) => {
+    try {
+        db.executeQuery(`select * from cocodevs order by id desc`, results => res.json(results));
+    } catch (e) {
+        console.log('Error logged......', e);
         res.sendStatus(5000);
     }
 });
 
-router.post('/cocodevs/post',(req,res,next)=>{
-    try{
+router.post('/cocodevs/post', (req, res, next) => {
+    try {
         const data = req.body;
-        const url=data.url;
-        const name=data.name;
-        const title=data.title;
-        const description=data.description;
-        db.executeQuery(`INSERT INTO cocodevs (url, name,title,description) VALUES ("${url}","${name}","${title}","${description}")`,results=>res.json(results));
+        const url = data.url;
+        const name = data.name;
+        const title = data.title;
+        const description = data.description;
+        db.executeQuery(`INSERT INTO cocodevs (url, name,title,description) VALUES ("${url}","${name}","${title}","${description}")`, results => res.json(results));
 
-    }catch(e){
-        console.log('Error logged......',e);
+    } catch (e) {
+        console.log('Error logged......', e);
         res.sendStatus(5000);
     }
 })
 
-module.exports=router;
+module.exports = router;
