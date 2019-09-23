3c62afbda7562761d217100e8fa98814d9d80de2
Abhishek Jha <abhishekjha@Abhisheks-MacBook-Pro.local>
2019-09-23 19:48:02 +0530

    Test

--- server/routes/index.js
+++ server/routes/index.js
@@ -86,10 +86,11 @@ router.post('/test/branch', (req, res, next) => {
             try {
                 // check if there is any change or not
                 let changeExist = executeCommand('git diff --exit-code && git diff --cached --exit-code');
-                console.log(changeExist);
+                if (changeExist)
+                    return res.status(405).json({ "error": true, "message": "Please commit your changes or stash them" });
                 const simpleGit = require('simple-git')('./');
                 simpleGit.fetch(branch, () => {
-                    try { 
+                    try {
                         simpleGit.checkout(branch, () => {
                             // TODO Command to run pm2
                             executeCommand('cd .. && cd client && npm i && npm run build');
